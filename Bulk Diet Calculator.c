3f47fbd22e1a1399c95435ed901fbcf3f778f02a
Mayibongwe Nkosenhle Ndlovu <165955583+MJ14icon@users.noreply.github.com>
2026-07-07 16:27:52 +0100

    Add files via upload
    These are simplest most basic codes for the two applications of the App

---
+++ Bulk Diet Calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,f;
+    printf("Welcome to The Wrld Diet Calculator\n");
+    printf("Please enter your current weight in kilograms:\n");
+    scanf("%d", &a);
+    b=a*2.2;
+    printf("Your weight in Pounds is:%d\n", b);
+    c=b*18;
+    printf("You should be eating a total of %d Calories per day\n", c);
+    d=a*2.2;
+    printf("You need %d grams of Protein minimum each day\n", d);
+    e=b*1.6;
+    printf("You should be eating %d grams of Carbs each day\n", e);
+    f=b*0.8;
+    printf("You should be eating %d grams of Fat each day\n", f);
+    printf("Stick to these Measurements and you should reach your body goals in no time\n");
+    return 0;
+}
